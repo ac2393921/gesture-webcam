d8c303fcaec407a5d2a3de5833fcd6bcb86c738f
ac2393921 <ac2393921@gmail.com>
2021-12-24 20:51:48 +0900

    update: move netのdetect結果をレンダリングする機能の追加

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,184 +1,57 @@
-import { image } from "@tensorflow/tfjs-core";
-import useSWR from 'swr';
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Webcam from "react-webcam";
 import 'tailwindcss/tailwind.css';
+import Header from "../components/Header";
+import RapList from "../components/RapList";
 
 const IndexPage = () => {
   const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
-
-  const test = () => {
-    console.log('test1');
-    const res = fetch('/api/pose', {
-      body: JSON.stringify({
-        email: 'aaa',
-        message: 'bbb'
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST'
-    }).then(function(json) {
-      console.log(json)
-    })
-    console.log(res);
-    console.log('test2');
-  }
+  const [image, setImage] = useState(null);
 
   const runDetect = async () => {
-    setInterval(() => {
+    setInterval(async () => {
       const imageSrc = webcamRef.current.getScreenshot();
-      const formData = new FormData()
-      formData.append('image', imageSrc)
+      if (imageSrc) {
+        const base64 = imageSrc.split(',')[1];
 
-      const res = fetch('/api/pose', {
-        body: JSON.stringify({
-          email: 'aaa',
-          message: 'bbb'
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        method: 'POST'
-      })
-      // const data = fetch('/api/pose', imageSrc);
-      // const response = await fetch('/api/pose');
-      // detect(imageSrc);
-    }, 10);
+        const res = await fetch('/api/pose', {
+          body: JSON.stringify({ image: base64 }),
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          method: 'POST'
+        })
+        const data = await res.json();
+        setImage(data.image);
+      }
+    }, 1000);
   };
 
-  // const detect = (image) => {
-  //   const formData = new FormData()
-  //   formData.append('image', image)
-
-  //   const apiRes = fetch("http://localhost:5000/pose/detect", {
-  //       method: 'POST',
-  //       headers: {
-  //       'Content-Type': 'multipart/form-data',
-  //     },
-  //     body: formData
-  //   }).then(response => {
-  //     if (!response.ok) {
-  //       console.error('サーバーエラー');
-  //     }
-  //     // ここに成功時の処理を記述
-  //   })
-  //   .catch(error => {
-  //     console.error('通信に失敗しました', error);
-  //   });
-
-  //   console.log(apiRes);
-  // }
-
-  // const runPosenet = async () => {
-  //   const net = await posenet.load();
-  //   //
-  //   setInterval(() => {
-  //     detect(net);
-  //   }, 100);
-  // };
-
-  // const detect = async (net) => {
-  //   if (
-  //     typeof webcamRef.current !== "undefined" &&
-  //     webcamRef.current !== null &&
-  //     webcamRef.current.video.readyState === 4
-  //   ) {
-  //     // Get Video Properties
-  //     const video = webcamRef.current.video;
-  //     const videoWidth = webcamRef.current.video.videoWidth;
-  //     const videoHeight = webcamRef.current.video.videoHeight;
-
-  //     // Set video width
-  //     webcamRef.current.video.width = videoWidth;
-  //     webcamRef.current.video.height = videoHeight;
-
-  //     // Make Detections
-  //     const pose = await net.estimateSinglePose(video);
-  //     console.log(pose);
-
-  //     drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
-  //   }
-  // };
-
-  // const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
-  //   const ctx = canvas.current.getContext("2d");
-  //   canvas.current.width = videoWidth;
-  //   canvas.current.height = videoHeight;
-
-  //   drawKeypoints(pose["keypoints"], 0.6, ctx);
-  //   drawSkeleton(pose["keypoints"], 0.7, ctx);
-  // };
-
-  // const detect = async (net) => {
-  //   if (
-  //     typeof webcamRef.current !== "undefined" &&
-  //     webcamRef.current !== null &&
-  //     webcamRef.current.video.readyState === 4
-  //   ) {
-  //     const video = webcamRef.current.video;
-  //     const videoWidth = webcamRef.current.video.videoWidth;
-  //     const videoHeight = webcamRef.current.video.videoHeight;
-
-  //     const imageSrc = webcamRef.current?.getScreenshot();
-  //     // console.log(imageSrc)
-
-  //     webcamRef.current.video.width = videoWidth;
-  //     webcamRef.current.video.height = videoHeight;
-
-  //     canvasRef.current.width = videoWidth;
-  //     canvasRef.current.height = videoHeight;
-
-  //     const face = await net.estimateFaces({ input: video });
-
-  //     // const ctx = canvasRef.current.getContext("2d");
-  //     // requestAnimationFrame(() => { drawMesh(face, ctx) });
-  //   }
-  // };
-
-  // useEffect(() => { runDetect() }, []);
+  useEffect(() => { runDetect() }, []);
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <div>
-        <div>
-          <div style={{
-                position: "relative",
-                zIndex: 9,
-                width: 640,
-                height: 480,
-                marginBottom: 20
-              }}>
+      <Header></Header>
+
+      <section className="text-gray-400 bg-gray-800 body-font">
+        <div className="container mx-auto flex px-1 pb-20 pt-14 md:flex-row flex-col items-center">
+          <div className="relative" style={{
+            width: 640,
+            height: 480,
+          }}>
             <Webcam
               ref={webcamRef}
               style={{
                 position: "absolute",
-                marginLeft: 20,
-                // marginRight: "auto",
-                marginTop: 20,
-                left: 0,
-                right: 0,
-                textAlign: "center",
                 zIndex: 9,
                 width: 640,
                 height: 480,
               }}
             />
-
-            <canvas
-              ref={canvasRef}
+            <img
+              src={image}
               style={{
                 position: "absolute",
-                marginLeft: 20,
-                // marginRight: "auto",
-                marginTop: 20,
-                left: 0,
-                right: 0,
-                textAlign: "center",
                 zIndex: 9,
                 width: 640,
                 height: 480,
@@ -186,17 +59,9 @@ const IndexPage = () => {
             />
           </div>
 
-          <div>
-            <button onClick={test}>Start</button>
-            <button>Stop</button>
-            <button>Reset</button>
-          </div>
+          <RapList></RapList>
         </div>
-
-        <div>
-          aaaa
-        </div>
-      </div>
+      </section>
     </div>
   );
 }
